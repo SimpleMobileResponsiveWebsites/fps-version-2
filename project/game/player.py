43dfeaa55ea5f87479ef68f7b13122ad09c0c7e0
Nathan Rossow <nathanrossow@gmail.com>
2024-12-08 19:41:32 -0600

    Update player.py

--- project/game/player.py
+++ project/game/player.py
@@ -1,6 +1,9 @@
+"""Player management including movement, camera, and inventory"""
 from panda3d.core import LVector3, Point3
 from direct.task.Task import Task
 from .utils import create_debug_text
+from .inventory import PlayerInventory, Equipment
+from .gui_manager import EquipmentGUI
 
 class Player:
     def __init__(self, game):
@@ -10,6 +13,13 @@ class Player:
         self.mouse_sensitivity = 0.2
         self.movement_speed = 5
         self.setup_debug_display()
+        
+        # Initialize inventory and GUI
+        self.inventory = PlayerInventory()
+        self.equipment_gui = EquipmentGUI(game)
+        
+        # Add some test equipment
+        self.add_test_equipment()
     
     def setup_camera(self):
         self.game.disableMouse()
@@ -25,6 +35,22 @@ class Player:
     def setup_debug_display(self):
         """Setup debug information display"""
         self.pos_text = create_debug_text("", Point3(-1.3, 0, 0.9))
+    
+    def add_test_equipment(self):
+        """Add some test equipment to demonstrate the system"""
+        test_items = [
+            Equipment("Combat Helmet", "helmet", protection=25),
+            Equipment("ComTac 4", "headphones"),
+            Equipment("M4A1", "weapon_1"),
+            Equipment("Glock 17", "handgun"),
+            Equipment("Combat Knife", "melee")
+        ]
+        
+        for item in test_items:
+            self.inventory.equip(item)
+        
+        # Update GUI
+        self.equipment_gui.update_equipment_display(self.inventory)
     
     def update(self):
         self.process_mouse()
@@ -64,3 +90,10 @@ class Player:
         """Update debug information"""
         pos = self.camera.getPos()
         self.pos_text.setText(f"Pos: {pos.x:.1f}, {pos.y:.1f}, {pos.z:.1f}")
+    
+    def pickup_item(self, item: Equipment):
+        """Handle picking up new equipment"""
+        if self.inventory.equip(item):
+            self.equipment_gui.update_equipment_display(self.inventory)
+            return True
+        return False
